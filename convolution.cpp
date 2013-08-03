088fdd21dfbf6a29f78c9727928f188bc82e7727
Sakares Saengkaew <jobbeins@gmail.com>
2013-08-03 13:52:21 +0700

    Refactor corrected logic in convolution.cpp

--- convolution.cpp
+++ convolution.cpp
@@ -1,15 +1,3 @@
-///////////////////////////////////////////////////////////////////////////////
-// convolution.cpp
-// ===============
-// convolution 1D
-//
-//  AUTHOR: Song Ho Ahn
-// CREATED: 2005-07-18
-// UPDATED: 2005-09-12
-//
-// Copyright (c) 2005 Song Ho Ahn
-///////////////////////////////////////////////////////////////////////////////
-
 #include <cmath>
 #include "convolution.h"
 
@@ -19,12 +7,28 @@
 ///////////////////////////////////////////////////////////////////////////////
 bool convolve1D(float* in, float* out, int dataSize, float* kernel, int kernelSize)
 {
-    int i;
-    
-    // Generate static data to make test runnint without error first
-    for(i = 0; i < dataSize; ++i)
+    int i, j, k;
+
+    // check validity of params
+    if(!in || !out || !kernel) return false;
+    if(dataSize <=0 || kernelSize <= 0) return false;
+
+    // start convolution from out[kernelSize-1] to out[dataSize-1] (last)
+    for(i = kernelSize-1; i < dataSize; ++i)
     {
-        out[i] = 0.0;
+        out[i] = 0;                             // init to 0 before accumulate
+
+        for(j = i, k = 0; k < kernelSize; --j, ++k)
+            out[i] += in[j] * kernel[k];
+    }
+
+    // convolution from out[0] to out[kernelSize-2]
+    for(i = 0; i < kernelSize - 1; ++i)
+    {
+        out[i] = 0;                             // init to 0 before sum
+
+        for(j = i, k = 0; j >= 0; --j, ++k)
+            out[i] += in[j] * kernel[k];
     }
 
     return true;
